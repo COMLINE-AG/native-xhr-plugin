70b18ccc29e3d78ac7b90ea3e716a1432d64df4f
Tim Huelsmeier <tim.huelsmeier@comlineag.de>
2017-11-30 14:08:01 +0100

    C25

--- www/NativeXHR.js
+++ www/NativeXHR.js
@@ -1,12 +1,15 @@
 var NativeXHR = function () {
 };
 
-NativeXHR.executeXHR = function (xhrURL, errorCallback) {
+NativeXHR.executeXHR = function (xhrURL, successCallback, errorCallback) {
+	if (typeof successCallback !== 'function') {
+		throw new Error('NativeXHR Error: errorCallback is not a function. Its a ' + typeof errorCallback);
+	}
 	if (typeof errorCallback !== 'function') {
 		throw new Error('NativeXHR Error: errorCallback is not a function. Its a ' + typeof errorCallback);
 	}
 
-	return cordova.exec(null, errorCallback, 'NativeXHR', 'executeXHR', [xhrURL]);
+	return cordova.exec(successCallback, errorCallback, 'NativeXHR', 'executeXHR', [xhrURL]);
 };
 
 module.exports = NativeXHR;
